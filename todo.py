4df6f5fc26233616590ebe860101d91c7b481ed6
Kyle Luck <luck.kyle@gmail.com>
2016-07-26 13:07:22 -0400

    extracting function to create query string

--- todo.py
+++ todo.py
@@ -29,29 +29,45 @@ def addtask():
 def processtasks():
     # if we're marking tasks as complete
     if 'markcomplete' in request.form:
-        for task in request.form:
-            if task == 'markcomplete':
-                continue
-            task = int(task)
-            db.update('tasks', {'id': task, 'complete': True})
+        tasks = create_task_list(request.form, 'markcomplete')
+        query = "update tasks set complete = True where id in (%s)" % create_db_in_string(tasks)
+        db.query(query, *tasks)
 
     # if we're deleting tasks
     if 'deletetasks' in request.form:
-        for task in request.form:
-            if task == 'deletetasks':
-                continue
-            task = int(task)
-            db.delete('tasks', {'id': task})
+        tasks = create_task_list(request.form, 'deletetasks')
+        query = "delete from tasks where id in (%s)" % create_db_in_string(tasks)
+        db.query(query, *tasks)
+        # old way:
+        # for task in request.form:
+        #     if task == 'deletetasks':
+        #         continue
+        #     tasks.append(int(task))
+        #     task = int(task)
+        #     db.delete('tasks', {'id': task})
 
-    # if we're marking tasks as not complete from complete
+    # if we're marking tasks as NOT complete
     if 'marknotcomplete' in request.form:
-        for task in request.form:
-            if task == 'marknotcomplete':
-                continue
-            task = int(task)
-            db.update('tasks', {'id': task, 'complete': False})
+        tasks = create_task_list(request.form, 'marknotcomplete')
+        query = "update tasks set complete = False where id in (%s)" % create_db_in_string(tasks)
+        db.query(query, *tasks)
 
     return redirect('/')
+
+def create_task_list(request_form, action):
+    tasks = []
+    for task in request_form:
+        if task == action:
+            continue
+        tasks.append(int(task))
+    return tasks
+
+def create_db_in_string(tasks):
+    string = ''
+    for i in xrange(0, len(tasks)):
+        string += "$" + str(i + 1) + ", "
+    return string.strip(', ')
+
 
 if __name__ == "__main__":
     app.debug = True
